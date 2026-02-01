058ec2a7a93c51c4e5218105a6a0f6abeca97718
NewUser2024 <newuser2024@Venkateshs-MacBook-Air.local>
2026-02-01 17:57:06 +0530

    Add frame POST endpoint

--- server.py
+++ server.py
@@ -1,7 +1,39 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
+import json
 
 class SimpleHandler(BaseHTTPRequestHandler):
+
+    def do_POST(self):
+        if self.path != "/frame":
+            self.send_response(404)
+            self.end_headers()
+            return
+
+        content_length = int(self.headers.get("Content-Length", 0))
+        body = self.rfile.read(content_length)
+
+        try:
+            frame = json.loads(body.decode())
+            frame_id = frame["id"]
+        except Exception:
+            self.send_response(400)
+            self.end_headers()
+            return
+
+        response_frame = {
+            "id": frame_id + 1
+        }
+
+        response_data = json.dumps(response_frame).encode()
+
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Content-Length", str(len(response_data)))
+        self.end_headers()
+
+        self.wfile.write(response_data)
+
     def do_GET(self):
         self.send_response(200)
         self.send_header("Content-Type", "text/plain")
